51a9c0c69fb045d154c493bc706ee81237d8bc7b
Jonathan D. Storm <jonathan.dale.storm@protonmail.ch>
2015-04-21 19:58:20 -0500

    Added IPCIDRRouteEntry

--- lib/ip_forward/ip_cidr_route_table.ex
+++ lib/ip_forward/ip_cidr_route_table.ex
@@ -13,15 +13,63 @@ defmodule IPForward.IPCIDRRouteTable do
     @oid_ipCidrRouteEntry               "1.3.6.1.2.1.4.24.4.1"
 
     @oid_ipCidrRouteDest                {"#{@oid_ipCidrRouteEntry}.1", :string}
+    @oid_ipCidrRouteMask                {"#{@oid_ipCidrRouteEntry}.2", :string}
+    @oid_ipCidrRouteNextHop             {"#{@oid_ipCidrRouteEntry}.4", :string}
 
     defstruct [
+      ipCidrRouteDest: nil,
+      ipCidrRouteMask: nil,
+      ipCidrRouteNextHop: nil
     ]
-    @type t :: %{
+    @type t :: %IPCIDRRouteEntry{
+      ipCidrRouteDest: String.t,
+      ipCidrRouteMask: String.t,
+      ipCidrRouteNextHop: String.t
     }
+
+    def ipCidrRouteDest do
+      ipCidrRouteDest(%IPCIDRRouteEntry{})
+    end
+    def ipCidrRouteDest(ipCidrRouteEntry) do
+      {oid, type} = @oid_ipCidrRouteDest
+
+      SNMPMIB.object(oid, type, ipCidrRouteEntry.ipCidrRouteDest)
+    end
+    def ipCidrRouteDest(ipCidrRouteEntry, value) when is_binary(value) do
+      %IPCIDRRouteEntry{ipCidrRouteEntry|ipCidrRouteDest: value}
+    end
+
+    def ipCidrRouteMask do
+      ipCidrRouteMask(%IPCIDRRouteEntry{})
+    end
+    def ipCidrRouteMask(ipCidrRouteEntry) do
+      {oid, type} = @oid_ipCidrRouteMask
+
+      SNMPMIB.object(oid, type, ipCidrRouteEntry.ipCidrRouteMask)
+    end
+    def ipCidrRouteMask(ipCidrRouteMask, value) when is_binary(value) do
+      %IPCIDRRouteEntry{ipCidrRouteEntry|ipCidrRouteMask: value}
+    end
+
+    def ipCidrRouteNextHop do
+      ipCidrRouteNextHop(%IPCIDRRouteEntry{})
+    end
+    def ipCidrRouteNextHop(ipCidrRouteEntry) do
+      {oid, type} = @oid_ipCidrRouteNextHop
+
+      SNMPMIB.object(oid, type, ipCidrRouteEntry.ipCidrRouteNextHop)
+    end
+    def ipCidrRouteNextHop(ipCidrRouteNextHop, value) when is_binary(value) do
+      %IPCIDRRouteEntry{ipCidrRouteEntry|ipCidrRouteNextHop: value}
+    end
   end
 
-  def ip_cidr_route_entry do
+  @spec ip_cidr_route_entry(String.t, String.t, String.t) :: IPCIDRRouteEntry.t
+  def ip_cidr_route_entry(destination, net_mask, next_hop) do
     %IPCIDRRouteEntry{}
+      |> IPCIDRRouteEntry.ipCidrRouteDest(destination)
+      |> IPCIDRRouteEntry.ipCidrRouteMask(net_mask)
+      |> IPCIDRRouteEntry.ipCidrRouteNextHop(next_hop)
   end
 end
 
